096d121f376a121334bed7b817320be564d07f3e
Mimona89 <152833108+Mimona89@users.noreply.github.com>
2023-12-04 10:19:04 +0600

    Create JavaApplication14.java

---
+++ JavaApplication14.java
@@ -0,0 +1,15 @@
+package javaapplication144;
+
+
+public class JavaApplication144 {
+
+    
+    public static void main(String[] args) {
+int a= 20 ,b=  5 ;
+int sum = a + b ;     
+        
+        System.out.print("\n***********  Mimona Akter ************\n");
+        System.out.print("\nSum is  = \n"+sum);
+    }
+    
+}
